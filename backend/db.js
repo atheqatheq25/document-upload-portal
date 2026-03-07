c5ff1379a95f6eae6fa4efc12c570f651aba32ee
atheqatheq25 <atheqsocial@gmail.com>
2026-03-07 13:02:45 +0530

    final

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "document_portal"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
